b067b5baa0f5512cf1bf2e9df10d3886178c64f5
i7-8700 <1281261856@qq.com>
2020-04-13 22:21:54 +0800

    update

---
+++ mxygamedev/005.timer/005.timer.cpp
@@ -0,0 +1,126 @@
+#include <Windows.h>
+#include "resource.h"
+
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "MSImg32.lib")
+
+constexpr const auto WINDOW_CLASS = L"LEARN_DX";
+constexpr const auto WINDOW_TITLE = L"LearnDX - 005.Timer Sample";
+constexpr auto WINDOW_WIDTH = 356;
+constexpr auto WINDOW_HEIGHT = 200;
+constexpr auto BMP_COUNT = 11;
+
+
+HDC hdc = nullptr;
+HBITMAP bmps[BMP_COUNT] = {};
+int index = 0;
+
+ATOM myRegisterClass(HINSTANCE hInstance);
+bool myCreateWindow(HINSTANCE hInstance, int show);
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wp, LPARAM lp);
+void myPaint(HWND hwnd, HDC dc);
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, int show)
+{
+	myRegisterClass(hInstance);
+	if (!myCreateWindow(hInstance, show)) {
+		return 1;
+	}
+
+	MSG msg;
+	while (GetMessage(&msg, nullptr, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessageW(&msg);
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wp, LPARAM lp)
+{
+	PAINTSTRUCT ps;
+	HDC dc;
+	RECT rc;
+
+	switch (message) {
+	case WM_PAINT:
+		dc = BeginPaint(hwnd, &ps);
+		myPaint(hwnd, dc);
+		EndPaint(hwnd, &ps);
+		break;
+
+	case WM_DESTROY:
+		DeleteDC(hdc); hdc = nullptr;
+		for (int i = 0; i < BMP_COUNT; i++) {
+			DeleteObject(bmps[i]);
+		}
+		memset(bmps, 0, sizeof(bmps));
+		PostQuitMessage(0);
+		break;
+
+	case WM_TIMER:
+		index = (index + 1) % BMP_COUNT;
+		GetClientRect(hwnd, &rc);
+		InvalidateRect(hwnd, &rc, TRUE);
+		break;
+
+	default:
+		return DefWindowProcW(hwnd, message, wp, lp);
+	}
+
+	return 0;
+}
+
+void myPaint(HWND hwnd, HDC dc)
+{
+	wchar_t text[1024];
+	wsprintf(text, L"%s - Bmp Index: %d", WINDOW_TITLE, index);
+	SetWindowTextW(hwnd, text);
+
+	HGDIOBJ old = SelectObject(hdc, bmps[index]);	
+	BitBlt(dc, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, hdc, 0, 0, SRCCOPY);
+	SelectObject(hdc, old);
+}
+
+ATOM myRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wc;
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbWndExtra = 0;
+	wc.cbClsExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = (HICON)LoadImageW(hInstance, MAKEINTRESOURCEW(IDI_ICON1), IMAGE_ICON, 0, 0, LR_DEFAULTSIZE);
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(LTGRAY_BRUSH);
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = WINDOW_CLASS;
+	wc.hIconSm = nullptr;
+	return RegisterClassExW(&wc);
+}
+
+bool myCreateWindow(HINSTANCE hInstance, int show)
+{
+	HWND hwnd = CreateWindow(WINDOW_CLASS, WINDOW_TITLE, WS_OVERLAPPEDWINDOW,
+							 CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT, nullptr, nullptr, hInstance, nullptr);
+	if (!hwnd) { return false; }
+
+	for (int i = 0; i < BMP_COUNT; i++) {
+		bmps[i] = (HBITMAP)LoadImageW(hInstance, MAKEINTRESOURCEW(IDB_BITMAP1 + i), IMAGE_BITMAP, WINDOW_WIDTH, WINDOW_HEIGHT, LR_DEFAULTCOLOR);
+	}
+
+	ShowWindow(hwnd, show);
+	UpdateWindow(hwnd);
+	HDC dc = GetDC(hwnd);
+	hdc = CreateCompatibleDC(dc);
+	myPaint(hwnd, hdc);
+	ReleaseDC(hwnd, dc);
+
+	PlaySoundW(MAKEINTRESOURCEW(IDR_WAVE1), hInstance, SND_RESOURCE | SND_ASYNC | SND_LOOP);
+
+	SetTimer(hwnd, 1, 100, nullptr);
+
+	return true;
+}
